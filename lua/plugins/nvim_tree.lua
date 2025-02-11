eb1361adeb2f109d656ff3384558662df847d464
pzzzyda <zhongyuan03@outlook.com>
2025-02-11 13:44:15 +0800

    uses nvim-tree instead of neo-tree

---
+++ lua/plugins/nvim_tree.lua
@@ -0,0 +1,41 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '<leader>e', ':NvimTreeToggle<CR>', desc = 'Toggle nvim-tree', silent = true },
+  },
+  opts = {
+    renderer = {
+      icons = {
+        padding = ' ',
+        symlink_arrow = '  ',
+        glyphs = {
+          default = '',
+          symlink = '',
+          bookmark = '',
+          modified = '',
+          hidden = '󰜌',
+          folder = {
+            arrow_closed = '',
+            arrow_open = '',
+            default = '',
+            open = '',
+            empty = '',
+            empty_open = '',
+            symlink = '',
+            symlink_open = '',
+          },
+          git = {
+            unstaged = '',
+            staged = '',
+            unmerged = '',
+            renamed = '',
+            untracked = '',
+            deleted = '',
+            ignored = '',
+          },
+        },
+      },
+    },
+  },
+}
